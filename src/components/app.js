25cdb78bfa33c257b5cc73828add2c1d7dcb8864
rebeccabuie <rebeccabuie@gmail.com>
2019-01-22 20:23:29 -0700

    set up birthday form component set up date picker with moment.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux</h2>
+        <BirthdayForm />
       </div>
     );
   }
---
+++ src/components/birthdayForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+
+
+class BirthdayForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            startDate: moment()
+        }
+
+    }
+
+    handleChange(data) {
+        this.setState({
+            startDate: date
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Birthday Form Component!</h1>
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        )
+    }
+}
+
+export default BirthdayForm;
